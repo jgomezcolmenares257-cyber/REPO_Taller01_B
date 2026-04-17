569a1d73835d822c4a4115f5a343eefd769d9ebf
jgomezcolmenares257@gmail.com <jgomezcolmenares257@gmail.com>
2026-04-17 15:08:05 -0400

    creando persistencia

--- persistencia/persistencia/Program.cs
+++ persistencia/persistencia/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Creado por SharpDevelop.
- * Usuario: usuario
+ * Usuario: JULIO
  * Fecha: 17/4/2026
  * Hora: 2:23 p. m.
  * 
@@ -16,8 +16,18 @@ namespace persistencia
 		{
 			Console.WriteLine("Hello World!");
 			
-			// TODO: Implement Functionality Here
-			
+			// directorio
+			string rutaRiz = Path .conbine(AppDomain.CurrentDomain.BaseDirectory,"DATOSIUJO");
+		    string rutareportes =  Path.Combine(rutaRiz, "reporte");
+
+	Console.Writeline(rutaRiz);
+		    Console.WriteLine(rutareportes);
+		    
+		    if(!Directory.Exists (rutareportes)){
+		    	Directory.createDirectory(rutareportes);
+		    	Console.WriteLine("directorio creado correctamente");
+		    }
+		    
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
